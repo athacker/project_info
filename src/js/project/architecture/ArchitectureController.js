8ada7033d9d8c37d5fb4b287cf3fbb85eb2f41be
athacker <andrea.thacker@outlook.net>
2015-11-23 16:00:27 -0600

    Initital Commit

--- src/js/project/architecture/ArchitectureController.js
+++ src/js/project/architecture/ArchitectureController.js
@@ -1,3 +1,26 @@
 /**
- * Created by AndreaThacker on 11/23/2015.
+ * Created by Andrea Thacker
  */
+(function(){
+
+    angular.module('app').controller('architectureController', function($scope ) {
+
+        var ac = this;
+        ac.title='';
+
+        ac.init=init();
+
+        function init(){
+            console.log('Initialize the Architcture Controller as ac');
+            ac.title = 'Project Architecture';
+
+        }
+    });
+
+
+
+
+
+
+
+})();
