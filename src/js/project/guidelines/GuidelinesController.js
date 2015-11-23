d4218aa9aeedfb4dfa13d7dd2b25c679f061f17f
athacker <andrea.thacker@outlook.net>
2015-11-23 15:59:56 -0600

    Initital Commit

--- src/js/project/guidelines/GuidelinesController.js
+++ src/js/project/guidelines/GuidelinesController.js
@@ -1,3 +1,21 @@
 /**
- * Created by AndreaThacker on 11/23/2015.
+ * Created by Andrea Thacker
  */
+(function(){
+    angular.module('app').controller('guidelinesController', function($scope ) {
+
+        var gc = this;
+        gc.title = '';
+        gc.init = init();
+
+
+        function init(){
+          console.log("Initialize Guidelines Controller as gc");
+          gc.title = 'Guidelines Controller';
+        };
+
+
+
+    });
+
+})();
