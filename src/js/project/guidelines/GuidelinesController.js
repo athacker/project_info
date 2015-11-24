a5cf47feeb023d349f96ee79c1a402148f527cdf
athacker <andrea.thacker@outlook.net>
2015-11-24 09:00:32 -0600

    Initital Commit

--- src/js/project/guidelines/GuidelinesController.js
+++ src/js/project/guidelines/GuidelinesController.js
@@ -4,14 +4,27 @@
 (function(){
     angular.module('app').controller('guidelinesController', function($scope ) {
 
+        /**
+         * Class Variables
+         */
         var gc = this;
         gc.title = '';
+
+
+
+        /**
+         * Method Interface
+         */
         gc.init = init();
 
 
+
+        /**
+         * Method implementation
+         */
         function init(){
           console.log("Initialize Guidelines Controller as gc");
-          gc.title = 'Guidelines Controller';
+          gc.title = 'Project Guidelines';
         }
 
 
