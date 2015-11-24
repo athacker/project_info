11391c8cb6503f9bb3805923c841a4fb33ae5f06
athacker <andrea.thacker@outlook.net>
2015-11-24 11:33:53 -0600

    Initital Commit

---
+++ src/js/project/shared/NavigateDirective.js
@@ -0,0 +1,24 @@
+/**
+ * Created by AndreaThacker on 11/24/2015.
+ */
+angular.module('app').directive('navigation', function(){
+
+    return{
+
+        restrict: 'E',
+        trasclude: true,
+        scope:{},
+        templateUrl: '/html/partials/navigation.html',
+
+        controller:  'mainController',
+        controllerAs: 'mc'
+
+
+
+
+
+
+    };//return
+
+
+});
