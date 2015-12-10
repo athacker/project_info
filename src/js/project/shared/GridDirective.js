28f0c910bbe0995684f62c766e6582dcdd7938de
athacker <andrea.thacker@outlook.net>
2015-12-10 14:01:42 -0600

    Update directive to enter new data.

---
+++ src/js/project/shared/GridDirective.js
@@ -0,0 +1,59 @@
+ /**
+ * Created by AndreaThacker on 11/24/2015.
+ */
+app.directive('grid', function(){
+    return{
+        restrict: 'E',
+
+        scope:{
+            remove: '&', accept: '&',  data: '=', title: '='
+        },
+        templateUrl: '/html/partials/grid.html',
+        controller: function($scope  ){
+            $scope.isEdit=false;
+            $scope.newItem={};
+            $scope.databackup=[];
+
+
+            //this isn't rednering fast enough!!'
+            angular.copy($scope.data, $scope.databackup);
+
+            $scope.saveNewItem=function(){
+                $scope.$broadcast('show-errors-check-validity');
+                if($scope.itemform.$valid){
+                    $scope.save($scope.newItem);
+                    $scope.addNew=false;
+                }
+            };
+
+            $scope.save= function(saveItem){
+              $scope.isEdit=false;
+              $scope.accept({item: saveItem});
+            },
+
+            $scope.delete = function(removeItem ){
+                $scope.remove( {item: removeItem});
+            };
+            
+            $scope.cancel=function(){
+                $scope.newItem={};
+                $scope.addNew=false;
+            };
+            $scope.cancelEdit=function( $index ){
+                $scope.data[$index].value = $scope.databackup[$index].value;
+            };
+
+            $scope.reset=function(){
+                $scope.newItem={};
+                $scope.$broadcast('show-errors-reset');
+            };
+
+            $scope.resetEdit=function( $index ){
+               $scope.data[$index].value = $scope.databackup[$index].value;
+            };
+        }
+
+    };//return
+
+
+});
---
+++ src/js/project/shared/NavigateDirective.js
@@ -0,0 +1,23 @@
+/**
+ * Created by AndreaThacker on 11/24/2015.
+ */
+app.directive('navigation', function(){
+
+    return{
+
+        restrict: 'E',
+        trasclude: true,
+        scope:{},
+        templateUrl: '/html/partials/navigation.html',
+        controller:  'mainController',
+        controllerAs: 'mc'
+
+
+
+
+
+
+    };//return
+
+
+});
